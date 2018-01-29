a61db3ae35090bca3979db45f8e9e66563c31afc
An Nuyen <annuyen@gmail.com>
2018-01-29 15:44:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here:
+function driversWithRevenueOver(driver, revenue) {
+  return driver.filter(function(element) {return element['revenue'] > revenue;});
+}
+
+function driverNamesWithRevenueOver() {
+  
+}
+
+function exactMatch() {
+  
+}
+
+function exactMatchToList() {
+  
+}
